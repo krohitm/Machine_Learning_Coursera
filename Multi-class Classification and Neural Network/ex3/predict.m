fc147da1aa4244f482b5798535344985f7e2d458
Kumar Rohit Malhotra <krohitm@gmail.com>
2016-11-02 02:13:51 -0400

    finished assignment 3

--- Multi-class Classification and Neural Network/ex3/predict.m
+++ Multi-class Classification and Neural Network/ex3/predict.m
@@ -10,6 +10,7 @@ num_labels = size(Theta2, 1);
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
 
+X = [ones(m,1) X];
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
 %               your learned neural network. You should set p to a 
@@ -21,7 +22,15 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+z2 = Theta1 * X.';
+a2 = sigmoid(z2);
 
+a2 = [ones(1, size(a2,2)); a2];
+
+z3 = Theta2 * a2;
+a3 = sigmoid(z3);
+[x,i] = (max(a3, [], 1));
+p = i.';
 
 
 
